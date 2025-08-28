bbc91100c6365d9e2fc475114faab9b764313e84
Jose Enrique Bobadilla <joseboba18@gmail.com>
2025-08-27 20:23:51 -0600

    fix base entity

--- src/common/base.entity.ts
+++ src/common/base.entity.ts
@@ -3,10 +3,4 @@ import { Column } from 'typeorm';
 export abstract class BaseEntity {
   @Column({ type: 'boolean', name: 'is_active' })
   public isActive: boolean;
-
-  @Column({
-    type: 'boolean',
-    name: 'is_deleted',
-  })
-  public isDeleted: boolean;
 }
